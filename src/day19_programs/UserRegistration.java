bf515bf264f5dfb64c75aff32e4dd9913bba7868
kumardinesh98 <devadineshkumar98@gmail.com>
2021-12-29 19:53:11 -0800

    email verification added

--- src/day19_programs/UserRegistration.java
+++ src/day19_programs/UserRegistration.java
@@ -6,20 +6,40 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	
+	Scanner scanner = new Scanner(System.in);
+	
 	public static void main(String[] args) {
+		UserRegistration userRegistration = new UserRegistration();
 		Scanner scanner = new Scanner(System.in);
-		String name ="^[A-Z]+[a-z]{3,}";
-		Pattern fname = Pattern.compile(name);
+		String validName ="^[A-Z]+[a-z]{3,}";
+		Pattern compiledName = Pattern.compile(validName);
 		System.out.println("enter the name");
 		String firstName = scanner.nextLine();
-		Matcher pinmatcher = fname.matcher(firstName);
+		userRegistration.check(compiledName, firstName);
+		System.out.println("enter last name");
+		String secondName = scanner.nextLine();
+		userRegistration.check(compiledName, secondName);
+		userRegistration.email();
+	}
+	public void email() {
+		System.out.println("enter email id");
+		String emailId = scanner.nextLine();
+		String validEmail = "^abc+.[a-z]*[@bl.co]+[.a-z]+";
+		Pattern compiledName = Pattern.compile(validEmail);
+		Matcher pinmatcher = compiledName.matcher(emailId);
 		if (pinmatcher.matches()) {
-			System.out.println(firstName + " fine -valid");
+			System.out.println(emailId + " is fine");
 		} else {
-			System.out.println(firstName + " not valid");
+			System.out.println(emailId + " not valid");
 		}
 	}
 	
+	public void check(Pattern con, String name) {
+		Matcher pinmatcher = con.matcher(name);
+		if (pinmatcher.matches()) {
+			System.out.println(name + " is fine");
+		} else {
+			System.out.println(name + " not valid");
+		}
+	}
 }
-
-
